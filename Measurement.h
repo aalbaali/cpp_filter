efebedd47868f532978e7edc99f26055ac934af5
aalbaali <amro.al-baali@mail.mcgill.ca>
2021-03-14 19:43:59 -0400

    Successfully importing data Measurement class (in header file)

---
+++ Measurement.h
@@ -0,0 +1,82 @@
+namespace Eigen{
+    // Vector template
+    template< size_t Size, typename T = double>
+    using Vector = Eigen::Matrix< T, Size, 1>;
+
+    template< size_t Size>
+    using Vectord = Vector< Size, double>;
+}
+
+// Class of measurements. Contains i) measurement, ii) covariance, and iii) time of the measurement.
+template<size_t Size, typename T = double>
+class Measurement{
+    public:
+        Measurement(){
+            _meas = Eigen::Matrix< T, Size, 1>::Zero();
+            _cov  = Eigen::Matrix< T, Size, Size>::Zero();
+            _t = -1.0;
+        }
+
+        Measurement( Eigen::Matrix< T, Size, 1> meas_in,
+            Eigen::Matrix< T, Size, Size> cov_in = Eigen::Matrix< T, Size, Size>::Identity(), double time_in = -1){
+            // Constructor that takes measurement and covariance (with default values).
+            _meas = meas_in;
+            _cov  = cov_in;
+            _t    = time_in;
+        }
+
+        template<typename VectorT>
+        Measurement( std::vector<VectorT> row_of_raw_data){
+            double time = row_of_raw_data[ 0];
+            Eigen::Vectord< Size> meas;
+            Eigen::Matrix< double, Size, Size> cov;
+            for( size_t i = 0; i < Size; i++){
+                meas( i) = row_of_raw_data[ i + 1];
+                for( size_t j = 0; j < Size; j++){
+                    cov( i, j) = row_of_raw_data[ 1 + Size + i * Size + j];
+                }
+            }
+            // Store objects
+            _t    = time;
+            _meas = meas;
+            // Ensure symmetry of the covariance matrix
+            _cov  =  cov;
+        }
+
+        // Getters
+        Eigen::Matrix< T, Size, 1> meas(){ return _meas;}
+        Eigen::Matrix< T, Size, Size> cov(){ return  _cov;}
+        double time(){ return _t;}
+
+        // Setters
+        void setMeas( Eigen::Matrix< T, Size, 1> meas_in){
+            this->_meas = meas_in;
+        }
+
+        void setCov( Eigen::Matrix<T, Size, 2> cov_in){
+            this->_cov = cov_in;
+        }
+        void setTime( double time_in){ _t = time_in;}
+
+    private:
+        Eigen::Matrix< double, Size, 1> _meas;
+        Eigen::Matrix< double, Size, Size> _cov;
+
+        // Time of measurement
+        double _t;
+};
+
+
+template<typename T>
+std::vector< T> ImportMeasurementsObjectVector(const std::string &file_name){
+    // Import raw vector data
+    auto raw_data = importData( file_name);
+    // Vector of measurement objects
+    std::vector< T> vector_of_measuremen_objects( raw_data.size());
+    for( int i = 0; i < raw_data.size(); i++){
+        // meas_control_input[ i] = getMeasurementObject< MeasControlInput, size_u>( raw_data[i]);
+        vector_of_measuremen_objects[ i] = T( raw_data[i]);
+    }
+
+    return vector_of_measuremen_objects;
+}
--- playground.cpp
+++ playground.cpp
@@ -1,104 +1,43 @@
 #include <iostream>
 #include <vector>
-
+#include <iomanip> // For nice outputs
 #include <unsupported/Eigen/MatrixFunctions>
 #include "Eigen/Dense"
 
 // Include parser
 #include "data_parser.h"
 
-namespace Eigen{
-    // Vector template
-    template< size_t Size, typename T = double>
-    using Vector = Eigen::Matrix< T, Size, 1>;
+#include "Measurement.h"
 
-    template< size_t Size>
-    using Vectord = Vector< Size, double>;
-}
-
-// Class of measurements. Contains i) measurement, ii) covariance, and iii) time of the measurement.
-template<size_t Size, typename T = double>
-class Measurement{
-    public:
-        Measurement(){
-            _meas = Eigen::Matrix< T, Size, 1>::Zero();
-            _cov  = Eigen::Matrix< T, Size, Size>::Zero();
-            _t = -1.0;
-        }
-
-        Measurement( Eigen::Matrix< T, Size, 1> meas_in,
-            Eigen::Matrix< T, Size, Size> cov_in = Eigen::Matrix< T, Size, Size>::Identity(), double time_in = -1){
-            // Constructor that takes measurement and covariance (with default values).
-            _meas = meas_in;
-            _cov  = cov_in;
-            _t    = time_in;
-        }
-
-        template<typename VectorT>
-        Measurement( std::vector<VectorT> row_of_raw_data){
-            double time = row_of_raw_data[ 0];
-            Eigen::Vectord< Size> meas;
-            Eigen::Matrix< double, Size, Size> cov;
-            for( size_t i = 0; i < Size; i++){
-                meas( i) = row_of_raw_data[ i + 1];
-                for( size_t j = 0; j < Size; j++){
-                    cov( i, j) = row_of_raw_data[ 1 + Size + i * Size + j];
-                }
-            }
-            // Store objects
-            _t    = time;
-            _meas = meas;
-            // Ensure symmetry of the covariance matrix
-            _cov  =  (1/2) * ( cov + cov.transpose());
-        }
-
-        // Getters
-        Eigen::Matrix< T, Size, 1> meas(){ return _meas;}
-        Eigen::Matrix< T, Size, Size> cov(){ return  _cov;}
-        double time(){ return _t;}
-
-        // Setters
-        void setMeas( Eigen::Matrix< T, Size, 1> meas_in){
-            this->_meas = meas_in;
-        }
-
-        void setCov( Eigen::Matrix<T, Size, 2> cov_in){
-            this->_cov = cov_in;
-        }
-        void setTime( double time_in){ _t = time_in;}
-
-    private:
-        Eigen::Matrix< double, Size, 1> _meas;
-        Eigen::Matrix< double, Size, Size> _cov;
-
-        // Time of measurement
-        double _t;
-};
-
+// Dimension of control input measurements
 const size_t size_u = 1;
-// Acceleration measurement class (takes a scalar input)
-typedef Measurement< size_u, double> MeasControlInput;
+// Dimension of the exeteroceptive measurement
+const size_t size_y = 1;
 
+// Acceleration measurement class
+typedef Measurement< size_u> MeasControlInput;
+typedef Measurement< size_y> MeasGPS;
 
 // Control input file name
 const std::string file_name_u = "/home/aalbaali/Documents/Code_base/Examples/Data_generator/linear_system/data/msd_acc.txt";
 
+const std::string file_name_gps = "/home/aalbaali/Documents/Code_base/Examples/Data_generator/linear_system/data/msd_pos.txt";
+
 int main(){
 
-    // Import vector data
-    auto data_vec = importData( file_name_u);
+    // ************************************************
+    // Import control input
+    std::vector< MeasControlInput> meas_control_input = ImportMeasurementsObjectVector<MeasControlInput>( file_name_u);
 
-    // Vector of measurement objects
-    std::vector< MeasControlInput> vec_meas_obj( data_vec.size());
-    for( int i = 0; i < data_vec.size(); i++){
-        // vec_meas_obj[ i] = getMeasurementObject< MeasControlInput, size_u>( data_vec[i]);
-        vec_meas_obj[ i] = MeasControlInput( data_vec[i]);
+    // ************************************************
+    // Import GPS measurements
+    std::vector< MeasGPS> meas_gps = ImportMeasurementsObjectVector<MeasGPS>( file_name_gps);
+
+    // Display measurements
+    std::cout << "Time\t\tMeas\t\tVar" << std::endl;
+    for( auto meas : meas_gps){
+        std::cout << std::setw(1) << meas.time() << "\t\t";
+        std::cout << std::setw(2) << meas.meas().transpose() << "\t\t";
+        std::cout << std::setw(5) << meas.cov() << std::endl;
     }
-    
-    // Extract the first row
-    auto first_meas = vec_meas_obj[10];
-    // Output the fields
-    std::cout << "time: " << first_meas.time() << std::endl;
-    std::cout << "meas: " << first_meas.meas().transpose() << std::endl;
-    std::cout << "cov: " << first_meas.cov() << std::endl;
 }
