ee5804dda8976eb2ba349c3268e8121ebb1f0ab3
albaalia <albaalia@live.com>
2021-04-18 00:43:29 -0400

    Doc: added EKF and parts of InEKF

--- InEKF/example_SE2.cpp
+++ InEKF/example_SE2.cpp
@@ -23,7 +23,7 @@ int main(int argc, const char* argv[]){
     //  GPS
     const std::string filename_gps   = config["filename_gps"].as<std::string>();
     // Estimated states
-    const std::string filename_out   = config["filename_out"].as<std::string>();
+    const std::string filename_out       std::string= config["filename_out"].as<std::string>();
     // If there's an argument, then read the YAML configuration file from input. Otherwise, use default directory
 
     // Import data
